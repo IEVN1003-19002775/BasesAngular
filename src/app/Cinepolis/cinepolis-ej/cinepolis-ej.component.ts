2f4dd96561ffa00d5ad0df4e43753cb1670158c1
Joan Morales <19002775@alumnos.utleon.edu.mx>
2024-09-23 14:34:03 -0600

    Cinepolis

---
+++ src/app/Cinepolis/cinepolis-ej/cinepolis-ej.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cinepolis-ej',
+  templateUrl: './cinepolis-ej.component.html',
+  styleUrl: './cinepolis-ej.component.css'
+})
+export class CinepolisEjComponent {
+
+  nombre!:string;
+  boletosCantidad: number = 1;
+  Cineco: boolean = false;
+  precioF: number | null = null;
+  precioBol: number = 12000;
+  maxBoletos: number = 7;
+  personasCantidad: number = 1;
+
+  MaxBoletos(): void {
+    this.maxBoletos = this.personasCantidad * 7;
+    if (this.boletosCantidad > this.maxBoletos) {
+      this.boletosCantidad = this.maxBoletos;
+    }
+  }
+
+  calcularPrecio(): void {
+    let desc = 0;
+
+    if (this.boletosCantidad > 5) {
+      desc = 0.15;
+    } else if (this.boletosCantidad >= 3 && this.boletosCantidad <= 5) {
+      desc = 0.10;
+    }
+
+    let precioTotal = this.precioBol * this.boletosCantidad;
+
+    precioTotal = precioTotal - (precioTotal * desc);
+
+    if (this.Cineco) {
+      precioTotal = precioTotal - (precioTotal * 0.10);
+    }
+
+    this.precioF = precioTotal;
+  }
+
+}
